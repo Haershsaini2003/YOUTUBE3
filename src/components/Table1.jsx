39bd132b6b8ae209e4f56c78bd5e30696dc47bd6
Haershsaini2003 <149783317+Haershsaini2003@users.noreply.github.com>
2023-11-19 22:08:33 +0530

    Add files via upload

---
+++ src/components/Table1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Table1 = () => {
+  return (
+    <table>
+      <tr>
+        <th>name</th>
+      </tr>
+      <tr>
+        <td>prince </td>
+      </tr>
+    </table>
+  );
+};
+
+export default Table1;
